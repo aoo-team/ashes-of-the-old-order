1b0bba05c955e445b8937cad53900c883efee246
Greenbueller <pennygaming10@gmail.com>
2022-01-18 22:40:03 -0500

    Fuck the Romanians!

--- common/defines/AOO_Defines.lua
+++ common/defines/AOO_Defines.lua
@@ -6,5 +6,5 @@ NDefines.NGame.STATE_DATE = "2018.1.1.12"
 NDefines.NGame.END_DATE = "2040.1.1.1"
 
 NDefines_Graphics.NGraphics.MINIMUM_PROVINCE_SIZE_IN_PIXELS = 6
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_SMALL_TEX_MAX_SIZE = 1024
 NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_MAX_SIZE = 512
-NDefines_Graphics.NGraphics.COUNTRY_FLAG_SMALL_TEX_MAX_SIZE = 128
